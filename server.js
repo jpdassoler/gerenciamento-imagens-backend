016a1c98955fbb626f468b024819718f846d69c0
João Pedro Perdomo Dassoler <104994947+jpdassoler@users.noreply.github.com>
2024-11-21 14:51:40 -0300

    Conexão ao bd e modularização do código
    - Criação de conexão ao mongodb
    - Separação de responsabilidades em: routes, controllers e models

--- server.js
+++ server.js
@@ -1,41 +1,12 @@
 import express from 'express';
-
-const posts = [
-    {
-        id: 1,
-        descricao: 'Gato fofo',
-        imagem: 'https://placecats.com/louie/300/150'
-    },
-    {
-        id: 2,
-        descricao: 'Cachorro brincando',
-        imagem: 'https://placecats.com/louie/300/150'
-    },
-    {
-        id: 3,
-        descricao: 'Paisagem montanhosa',
-        imagem: 'https://placecats.com/louie/300/200'
-    }
-];
+import routes from './src/routes/postsRoutes.js';
 
 const app = express();
-app.use(express.json());
+routes(app);
 
 app.listen(3000, () => {
     console.log('Servidor escutando...');
 });
 
-app.get('/posts', (req, res) => {
-    res.status(200).json(posts);
-});
 
-function buscarPostPorId(id) {
-    return posts.findIndex((post) => {
-        return post.id === Number(id);
-    });
-};
 
-app.get('/posts/:id', (req, res) => {
-    const index = buscarPostPorId(req.params.id);
-    res.status(200).json(posts[index]);
-});
